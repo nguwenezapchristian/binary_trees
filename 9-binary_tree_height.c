50c8f26d28bb61f606d58ba24f8400d868649a55
elyton250 <nelysemathieu@gmail.com>
2024-01-04 19:05:49 +0200

    task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - calculates the height of the tree
+ * @tree:
+ * Retrun: the lenght of the tree or zero when no tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	const binary_tree_t *temp = tree;
+	size_t rt = 0, lt = 0;
+
+	if (tree == NULL)
+		return(0);
+
+	while (temp->left != NULL)
+	{
+		temp = temp->left;
+		lt++;
+	}
+
+	temp = tree;
+
+	while (temp->right != NULL)
+	{
+		temp = temp->right;
+		rt++;
+	}
+
+	if (rt > lt)
+		return (rt);
+	else
+		return (lt);
+}
