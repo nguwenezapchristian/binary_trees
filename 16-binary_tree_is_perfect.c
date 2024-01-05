be81988efd47321cb4f8b6a483c74a3ed8830844
elyton250 <nelysemathieu@gmail.com>
2024-01-05 21:42:49 +0200

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,25 +7,45 @@
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	const binary_tree_t *temp = tree;
-	int lt = 0, rt = 0;
+	int rt, lt; 
 
-	while (temp->left != NULL || temp->right != NULL)
-	{
-		if (temp->left == NULL)
-			return (0);
-		temp = temp->left;
-		lt++;
-	}
-	temp = tree;
-	while (temp->right != NULL || temp->left != NULL)
-	{
-		if (temp->right == NULL)
-			return (0);
-		temp = temp->right;
-		rt++;
-	}
-	if (rt == lt)
+	lt = binary_tree_height(tree->left);
+	rt = binary_tree_height(tree->right);
+
+	if (lt == rt)
 		return (1);
-	return (0);
+}
+
+
+/**
+ * binary_tree_height - calculates the height of the tree
+ * @tree: is the tree
+ * Return: the lenght of the tree or zero when no tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lt = 0, rt = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	lt = binary_tree_height(tree->left);
+	rt = binary_tree_height(tree->right);
+	return (1 + (rt > lt ? rt : lt));
+}
+
+/**
+ * binary_tree_is_leaf - checks if node is leaf
+ * @node: the pointer to the node
+ * Return: 1 if leaf and 0 else
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
 }
