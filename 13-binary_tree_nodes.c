a924e5ae594018342426c25330c531d3cf299d61
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-05 15:27:31 +0200

    removed trailing whitespace

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,7 +12,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
 	if (tree->left != NULL || tree->right != NULL)
 	{
 		sumL += binary_tree_nodes(tree->left);
